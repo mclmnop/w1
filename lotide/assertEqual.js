52a01a27d87adf93c6d334fe04514a674eb09094
mclmnop <m_charlebois@hotmail.com>
2021-02-01 22:05:48 +0000

    aseertion function with emojis

---
+++ lotide/assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🥳🥳🥳 Assertion passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("⛔⛔⛔ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("patate", "patate");
+assertEqual(1,1);
+assertEqual(1,14);
