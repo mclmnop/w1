856b97e0aa7e38bebf1ac6e688717a2deae0dd9f
mclmnop <m_charlebois@hotmail.com>
2021-02-01 23:15:05 +0000

    test function

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🥳🥳🥳 Assertion passed: " + actual + " === " + expected);
+    console.log(`🥳🥳🥳 Assertion passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("⛔⛔⛔ Assertion failed: " + actual + " !== " + expected);
+    console.log(`⛔⛔⛔ Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
