20a9d66b30ceee8da1ee1ab54b6b61464f3d969a
mclmnop <m_charlebois@hotmail.com>
2021-02-02 23:12:30 +0000

    compare 2 regular arrays and assert

---
+++ lotide/eqArrays.js
@@ -0,0 +1,35 @@
+/*create variable to hold the true or false result of the comparison
+Compare array elements one by one
+if final comparison result is true, return it
+
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥳🥳🥳 Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`⛔⛔⛔ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let compareArr = true;
+  for (let arrIndex = 0; arrIndex < array1.length; arrIndex ++) {
+    if (array1[arrIndex] !== array2[arrIndex]) {
+      compareArr = false;
+    }
+  }
+  return compareArr; 
+}
+
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("patate", "patate");
+assertEqual(1,1);
+assertEqual(1,14);
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, "cheval"]), true);
