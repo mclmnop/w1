2af94e7846ba328ffc413fd44e534f720b10279d
mclmnop <m_charlebois@hotmail.com>
2021-02-03 00:11:51 +0000

    compare 2 arrays

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,25 @@
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🥳🥳🥳 Assertion passed: ${array1} ===  ${array2}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let compareArr = true;
+  for (let arrIndex = 0; arrIndex < array1.length; arrIndex ++) {
+    if (array1[arrIndex] !== array2[arrIndex]) {
+      compareArr = false;
+    }
+  }
+  return compareArr;
+};
+
+
+//TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 4, "cheval"]);
+assertArraysEqual([1, 2, 3], []);
