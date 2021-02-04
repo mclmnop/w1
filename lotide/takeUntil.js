5ccbe092c234ca3d5f83ff30a2e6fcef2e343e0c
mclmnop <m_charlebois@hotmail.com>
2021-02-04 23:03:38 +0000

    callback to stop loop

---
+++ lotide/takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🥳🥳🥳 Assertion passed: ${array1} ===  ${array2}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let compareArr = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let arrIndex = 0; arrIndex < array1.length; arrIndex ++) {
+    if (array1[arrIndex] !== array2[arrIndex]) {
+      compareArr = false;
+    }
+  }
+  return compareArr;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const data3 = ["will", "be", "well", "for", "xmas"];
+const results3 = takeUntil(data3, x => x === 'well');
+assertArraysEqual(results3, ["will", "be", "well"]);
