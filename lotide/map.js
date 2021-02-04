7508f8347079ca2918587fb1b0cdf3aebc713621
mclmnop <m_charlebois@hotmail.com>
2021-02-04 22:39:31 +0000

    callback and map

---
+++ lotide/map.js
@@ -0,0 +1,39 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+const map = function(array, callback) {
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🥳🥳🥳 Assertion passed: ${array1} ===  ${array2}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let compareArr = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let arrIndex = 0; arrIndex < array1.length; arrIndex ++) {
+    if (array1[arrIndex] !== array2[arrIndex]) {
+      compareArr = false;
+    }
+  }
+  return compareArr;
+};
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', '2']);
