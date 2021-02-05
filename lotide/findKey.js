b03b4b2ee3c4c637d51b63ebcc14effadd4f4452
mclmnop <m_charlebois@hotmail.com>
2021-02-05 00:00:50 +0000

    looping through object properties

---
+++ lotide/findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥳🥳🥳 Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`⛔⛔⛔ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Noma");
