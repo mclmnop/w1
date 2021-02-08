ada4ba5d188a7dc4801e62a49f14a0c522c0836b
mclmnop <m_charlebois@hotmail.com>
2021-02-08 16:41:23 -0500

    refactore with mocha

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -1,12 +1,18 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-//TEST CODE
-const list = [5,0,7,"pumpkin",3,4,6,7];
-const result = middle(list);
-assertArraysEqual(result, ["pumpkin",3]);
-
-assertArraysEqual(middle([6,7,"word", "silex", 9]), ["word"]); //true
-assertArraysEqual(middle([6,7,"word", "silex", 9, 10]), ["word"]); //false
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); //true
-assertArraysEqual(middle([6,7,8,10]), [7,8]); //true
+describe("#middle", () => {
+  it("returns ['pumpkin', 3] for [5,0,7,'pumpkin',3,4,6,7]", () => {
+    assert.deepEqual(middle([5,0,7,'pumpkin',3,4,6,7]), ['pumpkin', 3])
+  });
+  it("returns ['word'] for [6,7,'word','silex',9]", () => {
+    assert.deepEqual(middle([6,7,'word','silex',9]), ['word'])
+  });
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3])
+  });
+  it("returns [7,8] for [6,7,8,10]", () => {
+    assert.deepEqual(middle([6,7,8,10]), [7,8])
+  });
+})
