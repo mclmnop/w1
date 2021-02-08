e06e348e52db620e40f155759ea7f919db5d2b89
mclmnop <m_charlebois@hotmail.com>
2021-02-08 16:15:19 -0500

    refactor with mocha chai

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,11 +1,14 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail  = require('../tail');
 
-
-assertArraysEqual(tail(["pomme", "poire", "ananas"]), "poire");
-assertArraysEqual(tail(["pomme", "poire", "ananas"]), ["poire", "ananas"]);
-assertArraysEqual(tail([1,2,3,4]), [2,3,4]);
-const originalArray = ["one", "two", "three"];
-const result = tail(originalArray);
-assertEqual(result.length, 3);
+describe("#tail", () => {
+  it("returns pomme,poire for ['pomme,'poire','ananas']", () => {
+    assert.deepEqual(tail(["pomme", "poire", "ananas"]), ["poire", "ananas"]);
+  });
+  it("returns [2,3,4] [1,2,3,4]", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4]);
+  });
+  it("returns empty array if array is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
