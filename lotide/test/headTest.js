4a17512939b4d76b941e6d43773b940d69f6082d
mclmnop <m_charlebois@hotmail.com>
2021-02-08 15:59:24 -0500

    using mocha chai

--- lotide/test/headTest.js
+++ lotide/test/headTest.js
@@ -1,9 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([1,2,3,4,5]), 2);
-assertEqual(head([1,"poil",3,4,5]), "poil");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["first"]), "first");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 1 for [1,2,3,4,5]", () => {
+    assert.strictEqual(head([1,2,3,4,5]), 1);
+  });
+  it("returns Hello for ['Hello', 'Lighthouse', 'labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'labs']), 'Hello');
+  });
+  it("returns undefined if array is empty", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
